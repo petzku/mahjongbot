petzku/mahjongbot

commit aab9991fa918296561b0cda0cea1a31e10ad151a

Add helper functions
hand_to_emoji split into two functions: hand_to_tiles and tiles_to_emoji
hand_to_emoji is a wrapper for both.

drop_to_first takes the rest of haystack after the first occurence of needle

app.js
@@ -1,7 +1,6 @@
 /*
  * A simple bot that replies with emoji versions of mahjong hands
  */
-
 const Discord = require("discord.js");
 const conf = require("./conf");
 
@@ -15,20 +14,32 @@ const hand_regex = /([0-9]+[psm]|[1-7]+z)+/g;
 const part_regex = /([0-9]+)([psm])|([1-7]+)z/g;
 const tile_regex = /[0-9][pms]|[1-7]z/;
 
-function hand_to_emoji(hand) {
+function tiles_to_emoji(tiles) {
   let res = "";
+  for (const t of tiles) {
+    const code = t + suit;
+    res += "<:" + code + ":" + emoji_codes[code] + ">";
+  }
+  return res;
+}
+
+function hand_to_tiles(hand) {
   let match;
+  let tiles = [];
 
   while ((match = part_regex.exec(hand)) !== null) {
     const tiles = match[1] || match[3];
     const suit = match[2] || 'z';
 
     for (const t of tiles) {
-      const code = t + suit;
-      res += "<:" + code + ":" + emoji_codes[code] + ">";
+      tiles.push(t + suit);
     }
   }
-  return res;
+  return tiles;
+}
+
+function hand_to_emoji(hand) {
+  return tiles_to_emoji(hand_to_tiles(hand));
 }
 
 function process_command(content) {
@@ -38,7 +49,7 @@ function process_command(content) {
     const dora_tile = hand_regex.exec(content)[0];
     msg += "ドラ" + (hand_to_emoji(dora_tile)) + "        ";
     // this should work...
-    msg += process_hand(content.substring(content.indexOf(dora_tile)+1));
+    msg += process_hand(content.drop_to_first(dora_tile));
     return msg;
   } else if (content.startsWith("hand")) {
     return process_hand(content);
@@ -60,14 +71,21 @@ function process_hand(content) {
   return msg;
 }
 
+function drop_to_first(haystack, needle) {
+  return haystack.substring(haystack.indexOf(needle)+1);
+}
+
 bot.on('message', message => {
   let content = message.content;
   if (content.startsWith(prefix)) {
     // remove the prefix
     content = content.replace(prefix, '');
-    message.channel.sendMessage(process_command(content));
+    const msg = process_command(content);
+    if (msg) {
+      message.channel.sendMessage(msg);
+    }
   } else if (content.includes(prefix)) {
-    const rest = content.substring(content.indexOf(prefix)+1);
+    const rest = content.drop_to_first(needle);
 
     // test that it was actually a hand, and not a random use of $prefix
     if (new RegExp("^" + part_regex.source).test(rest)) {

commit ca1d9df040760c64c807bca0853a3627db003b94

Fix bugs introduced in aab9991 tuba code

app.js
@@ -19,8 +19,7 @@ const tile_regex = /[0-9][pms]|[1-7]z/;
 function tiles_to_emoji(tiles) {
   let res = "";
   for (const t of tiles) {
-    const code = t + suit;
-    res += "<:" + code + ":" + emoji_codes[code] + ">";
+    res += "<:" + t + ":" + emoji_codes[t] + ">";
   }
   return res;
 }
@@ -30,11 +29,10 @@ function hand_to_tiles(hand) {
   let tiles = [];
 
   while ((match = part_regex.exec(hand)) !== null) {
-    const tiles = match[1] || match[3];
+    const nums = match[1] || match[3];
     const suit = match[2] || 'z';
-
-    for (const t of tiles) {
-      tiles.push(t + suit);
+    for (const n of nums) {
+      tiles.push(n + suit);
     }
   }
   return tiles;